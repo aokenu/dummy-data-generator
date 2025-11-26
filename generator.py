102b7fbb3fe8e9d7c4f0255d4ad52e55259aff3b
aokenu <34852856+aokenu@users.noreply.github.com>
2025-11-26 17:27:15 +0100

    Updated project file

--- generator.py
+++ generator.py
@@ -24,3 +24,4 @@ def load_schema(file_path):
 
 def generate_dummy_data_from_schema(schema):
     """Generate dummy data based on schema definition."""
+    table_name = schema.get("table_name", "dummy_table")
